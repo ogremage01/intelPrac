5e291351714d0281fd34e164a5a094405fc8b528
ogremage01 <curselife01@gmail.com>
2024-12-13 16:51:47 +0900

    컴퓨터에서 이클립스 활용해 깃 리파지터리 업데이트 연습

---
+++ 컴퓨터폴더/VarBasic21.java
@@ -0,0 +1,20 @@
+
+public class VarBasic21 {
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 200;
+		
+		
+		System.out.println("x: "+ x + "/y: "+y);
+		
+		int temp = 0;
+		temp = x;
+		x = y;
+		y = temp;
+		
+		System.out.println("x: "+ x + "/y: "+y);
+		
+		
+		}
+
+}
